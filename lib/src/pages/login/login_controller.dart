44c628588566452ee8bcba4d2b08847add068303
Khac Duy <2002nguyenkhacduy@gmail.com>
2023-09-16 17:37:35 +0700

    redo login page

--- lib/src/pages/login/login_controller.dart
+++ lib/src/pages/login/login_controller.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:get/get.dart';
 import 'package:resources/resources.dart';
+import 'package:sign_in_button/sign_in_button.dart';
 
 part 'login_binding.dart';
 part 'login_page.dart';
--- lib/src/pages/login/login_page.dart
+++ lib/src/pages/login/login_page.dart
@@ -6,22 +6,130 @@ class LoginPage extends GetView<LoginController> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      alignment: Alignment.center,
-      padding: EdgeInsets.all(AppThemeExt.of.majorPaddingScale(4)),
-      child: Column(
-        children: [
-          Image(image: R.pngs.login.image().image),
-          const Text(
-            'Hello',
-            style: TextStyle(fontSize: 28),
-          ),
-          SizedBox(height: AppThemeExt.of.majorMarginScale(4)),
-          const Text(
-            'Welcome to FAST CHATTER, where you can have a quick chat with you friends',
-            textAlign: TextAlign.center,
-            style: TextStyle(fontSize: 14),
-          ),
-        ],
+      decoration: BoxDecoration(
+          image: DecorationImage(
+              image: R.pngs.login.image().image, fit: BoxFit.cover)),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Stack(
+          children: [
+            const SizedBox(),
+            Container(
+              padding: EdgeInsets.only(
+                  left: AppThemeExt.of.majorPaddingScale(20 / 4),
+                  top: AppThemeExt.of.majorPaddingScale(90 / 4)),
+              child: const Text(
+                'Welcome\nBack',
+                style: TextStyle(color: Colors.white, fontSize: 33),
+              ),
+            ),
+            SingleChildScrollView(
+              child: Container(
+                padding: EdgeInsets.only(
+                    top: MediaQuery.of(context).size.height * 0.4),
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(
+                            left: AppThemeExt.of.majorPaddingScale(35 / 4),
+                            right: AppThemeExt.of.majorPaddingScale(35 / 4)),
+                        child: Column(
+                          children: [
+                            TextField(
+                              style: TextStyle(color: Colors.black),
+                              decoration: InputDecoration(
+                                  fillColor: Colors.grey.shade100,
+                                  filled: true,
+                                  hintText: "Email",
+                                  border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(10),
+                                  )),
+                            ),
+                            SizedBox(
+                              height: AppThemeExt.of.majorMarginScale(5),
+                            ),
+                            TextField(
+                              // controller: password,
+                              style: TextStyle(),
+                              obscureText: true,
+                              decoration: InputDecoration(
+                                  fillColor: Colors.grey.shade100,
+                                  filled: true,
+                                  hintText: "Password",
+                                  border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(10),
+                                  )),
+                            ),
+                            SizedBox(
+                              height: AppThemeExt.of.majorMarginScale(3),
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                const Text(
+                                  'Sign in',
+                                  style: TextStyle(
+                                      fontSize: 27,
+                                      fontWeight: FontWeight.w700),
+                                ),
+                                SizedBox(
+                                  height: AppThemeExt.of.majorMarginScale(14),
+                                ),
+                                CircleAvatar(
+                                  radius: 30,
+                                  backgroundColor: const Color(0xff4c505b),
+                                  child: IconButton(
+                                      color: Colors.white,
+                                      onPressed: () {},
+                                      icon: const Icon(
+                                        Icons.arrow_forward,
+                                      )),
+                                ),
+                              ],
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                TextButton(
+                                  onPressed: () {},
+                                  child: const Text(
+                                    'Sign Up',
+                                    textAlign: TextAlign.left,
+                                    style: TextStyle(
+                                        decoration: TextDecoration.underline,
+                                        color: Color(0xff4c505b),
+                                        fontSize: 18),
+                                  ),
+                                ),
+                                TextButton(
+                                    onPressed: () {},
+                                    child: const Text(
+                                      'Forgot Password',
+                                      style: TextStyle(
+                                        decoration: TextDecoration.underline,
+                                        color: Color(0xff4c505b),
+                                        fontSize: 18,
+                                      ),
+                                    )),
+                              ],
+                            ),
+                            SizedBox(
+                              height: AppThemeExt.of.majorMarginScale(5),
+                            ),
+                            SignInButton(
+                              Buttons.google,
+                              text: "Sign in with Google",
+                              onPressed: () {},
+                            )
+                          ],
+                        ),
+                      )
+                    ]),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
